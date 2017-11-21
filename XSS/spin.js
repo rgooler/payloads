d6d384e3d4c6d01c8d02bff8a31f9c5bdfb77649
Ryan Gooler <cheetahmorph@gmail.com>
2017-11-21 15:01:42 -0800

    Update and rename spin.js to spin.html

--- XSS/spin.js
+++
@@ -1,6 +0,0 @@
-/* 
- * Spin the whole webpage!
- */
- 
-javascript:(function(){style=document.createElement("style");
-style.innerHTML=".lol {transition: 120s linear;transform: rotate(21600deg);}";document.getElementsByTagName("head")[0].appendChild(style);var x=document.querySelectorAll("body");for (var i=0;i<x.length; i++){var a=x[i];a.classList.toggle("lol")}})();
